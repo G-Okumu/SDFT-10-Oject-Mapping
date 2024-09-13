c16fe20363b8c58c45541714bda3f7b7b69a2c0e
George-Okumu <gokumu12@gmail.com>
2024-09-13 06:52:46 +0300

    feat: new test case

--- test2.py
+++ test2.py
@@ -3,16 +3,31 @@
 ## Columns ? instance or instance attributes => instance attr
 ## tables ? instance or class itself => class itself
 from director import Director
+from movie import Movie
 
-# Director.drop_table();
+Director.drop_table();
 
-# Director.create_table();
+Director.create_table();
 
-# direct1 = Director('Frodo')
-# direct2 = Director('Francis Ford')
+direct1 = Director('Frodo')
+direct2 = Director('Francis Ford')
 
 
-# direct2.save();
-# direct1.save();
+direct2.save();
+direct1.save();
 
-print(Director.get_all_directors());
+# print(Director.get_all_directors());
+
+
+Movie.drop_table()
+Movie.create_table()
+
+# Create a movie
+
+lord_of_the_rings = Movie('Lord of the Rings', 'Fantasy', direct1.id);
+harry_porter = Movie('Harry Porter', 'Fantasy', direct2.id);
+GOT = Movie('Game Of Thrones', 'Fantasy & War', direct1.id)
+
+lord_of_the_rings.save()
+harry_porter.save()
+GOT.save()
